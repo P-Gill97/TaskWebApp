2311263c7652a838610b9bab71dbd89db4a51980
P-Gill97 <pgill@csus.edu>
2019-01-02 20:53:35 -0800

    finished forloop for innerHtml

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function fetchtasks(){
-  var tasks = JSON.parse(localStorage.getItem('tasks'))
-  var tasksListe = document.getElementsById('tasksList')
+function fetchtasks() {
+  var tasks = JSON.parse(localStorage.getItem("tasks"));
+  var tasksListe = document.getElementsById("tasksList");
 
-  tasksList.innerHTML = '';
+  tasksList.innerHTML = "";
 
-  for(var i=0 ; i < tasks.length; i++ ){
+  for (var i = 0; i < tasks.length; i++) {
     // get variables
     var id = tasks[i].id;
     var desc = tasks[i].Description;
@@ -12,8 +12,28 @@ function fetchtasks(){
     var subj = tasks[i].Subject;
     var status = tasks[i].status;
     //html output
-    tasksList.innerHTML += '<div class="well">' +
-                            '<h6> Task id: ' + id + '</h6>'+
-                            '<p><span class = "label label-info ">' + status + '</span></p>'
+    tasksList.innerHTML +=
+      '<div class="well">' +
+      "<h6> Task id: " +
+      id +
+      "</h6>" +
+      '<p><span class = "label label-info ">' +
+      status +
+      "</span></p>" +
+      "<h3>  " +
+      desc +
+      "</h3>" +
+      '<p><span class = "glyphicon glyphicon-time "></span>' +
+      Priority +
+      "</span></p>";
+    '<p><span class = "glyphicon glyphicon-user "></span>' +
+      Subject +
+      "</span></p>" +
+      // making button for done and remove function.
+      '<a href="#" onclick ="setStatusDone(\''+id+'\')"class="btn btn-warning">Mark done</a>' +
+      '<a href="#" onclick="deleteTask(\''+id+'\')" class="btn btn-danger">Remove</a>'+
+      '</div>';
+
+      ;
   }
 }
