77a0824284b1cf5d2aecfda18b5a071487e45aba
P-Gill97 <pgill@csus.edu>
2019-01-02 17:35:44 -0800

    added innerhtml to hold the variables for the tasks

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-function fetchIssues(){
+function fetchtasks(){
   var tasks = JSON.parse(localStorage.getItem('tasks'))
   var tasksListe = document.getElementsById('tasksList')
 
   tasksList.innerHTML = '';
 
   for(var i=0 ; i < tasks.length; i++ ){
+    // get variables
     var id = tasks[i].id;
     var desc = tasks[i].Description;
     var prior = tasks[i].Priority;
     var subj = tasks[i].Subject;
+    var status = tasks[i].status;
+    //html output
+    tasksList.innerHTML += '<div class="well">' +
+                            '<h6> Task id: ' + id + '</h6>'+
+                            '<p><span class = "label label-info ">' + status + '</span></p>'
   }
 }
