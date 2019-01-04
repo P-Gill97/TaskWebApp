7dee26b5a78c4cae1850a94284332abcec72ed61
P-Gill97 <pgill@csus.edu>
2019-01-03 18:52:50 -0800

    trying to fix bug. Made mistakes like not using correct method signitures for getElementById

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // event handeler
-document.getElementsById("taskInputForm").addEventLisener("submit", saveTask);
+document.getElementById("taskInputForm").addEventLisener('submit', saveTask);
 function saveTask(e) {
-  var taskDesc = document.getElementsById("taskDescInput").value;
-  var taskPriority = document.getElementsById("taskPriorityInput").value;
-  var taskSubject = document.getElementsById("taskSubjectInput").value;
+  console.log('Started save task');
+  var taskDesc = document.getElementById('taskDescInput').value;
+  var taskPriority = document.getElementById('taskPriorityInput').value;
+  var taskSubject = document.getElementById('taskSubjectInput').value;
   var taskId = chance.guid();
   var taskStatus = "Unfinished";
 
@@ -13,28 +14,31 @@ function saveTask(e) {
     priority: taskPriority,
     subject: taskSubject,
     status: taskStatus
-  }
-  if(localStorage.getItem('tasks') == null){
+  };
+  if (localStorage.getItem("tasks") == null) {
     var tasksArr = [];
     tasksArr.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-  }else{
-    var tasksArr = JSON.parse(localStorage.getItem('tasks'));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    var tasksArr = JSON.parse(localStorage.getItem("tasks"));
     tasksArr.push(task);
-    localStorage.setItem('tasks',JSON.stringify(tasksArr));
+    localStorage.setItem("tasks", JSON.stringify(tasksArr));
   }
-  document.getElementsById('taskInputForm').reset();
+  document.getElementById("taskInputForm").reset();
 
   fetchtasks();
 
   e.preventDefault();
 }
 function fetchtasks() {
-  var tasks = JSON.parse(localStorage.getItem("tasks"));
-  var tasksListe = document.getElementsById("tasksList");
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+  var tasksListe = document.getElementById('tasksList');
 
-  tasksList.innerHTML = "";
-
+  tasksListe.innerHTML = '';
+  // hot fix
+if(tasks ==null){
+  tasks = [];
+}
   for (var i = 0; i < tasks.length; i++) {
     // get variables
     var id = tasks[i].id;
@@ -44,22 +48,21 @@ function fetchtasks() {
     var status = tasks[i].status;
     //html output
     tasksList.innerHTML +=
+    console.log("innerHtml started ");
       '<div class="well">' +
-      "<h6> Task id: " +
-      id +
-      "</h6>" +
+      '<h6> Task id:'  + id + '</h6>' +
       '<p><span class = "label label-info ">' +
       status +
-      "</span></p>" +
-      "<h3>  " +
+      '</span></p>' +
+      '<h3>' +
       desc +
-      "</h3>" +
+      '</h3>' +
       '<p><span class = "glyphicon glyphicon-time "></span>' +
-      Priority +
-      "</span></p>" +
+      prior +
+      '</span></p>' +
       '<p><span class = "glyphicon glyphicon-user "></span>' +
-      Subject +
-      "</span></p>" +
+      subj +
+      '</span></p>' +
       // making button for done and remove function.
       '<a href="#" onclick ="setStatusDone(\'' +
       id +
@@ -69,4 +72,5 @@ function fetchtasks() {
       '\')" class="btn btn-danger">Remove</a>' +
       "</div>";
   }
+  console.log("innerHtml ended ");
 }
